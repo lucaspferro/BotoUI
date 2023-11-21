91b426a49caa449fb04c2ecd988fbafa97821718
Matheus Luís <matheus.bernardi@lnls.br>
2023-11-21 14:28:31 -0300

    new front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 // import DemoPage from '@/components/payments/page';
 import PvDataInterface from '@/components/pv-data-interface';
+import { Button } from '@/components/ui/button';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
 
 export default function Home() {
+  const { data: sessionData } = useSession();
+
   return (
     <>
       <Head>
@@ -12,25 +15,21 @@ export default function Home() {
         <link rel="icon" href="/icon.svg" />
       </Head>
 
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#9370DB] to-[#15162c]">
-        <div className="flex justify-center px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Boto<span className="text-[#2e026d]">PV</span>Alarm
-            <p className="text-sm font-extrabold tracking-tight flex-col items-center justify-centerr text-white sm:text-[1.5rem]">
-              Programmable<span className="text-[#2e026d]">PV</span> alarm
-            </p>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <div className="flex flex-col gap-8 ">
+          <h1 className="text-2xl font-medium sm:text-[5rem]">
+            Boto<span className="text-pink-600">PV</span>Alarm
           </h1>
+          <p className="text-sm font-light tracking-tight flex-col items-center justify-center text-center sm:text-[1.5rem]">
+            Programmable <span className="text-pink-600">PV</span> alarm
+          </p>
         </div>
-
-        <div className="flex min-h-screen  justify-center  ">
-          <div className=" grid justify-items-star gap-2 ">
+        {sessionData && (
+          <div className="flex h-full justify-center p-2 rounded-lg shadow-lg">
             <PvDataInterface />
           </div>
-        </div>
-
-        <div className="flex flex-col items-center gap-2">
-          <AuthShowcase />
-        </div>
+        )}
+        <AuthShowcase />
       </main>
     </>
   );
@@ -38,18 +37,31 @@ export default function Home() {
 
 function AuthShowcase() {
   const { data: sessionData } = useSession();
+  const parseInitials = (email: string) => {
+    const noAt = email.split('@')[0] ?? 'no email';
+    const parsed = noAt
+      .split('.')
+      .map((n) => n[0])
+      .join('');
+
+    return parsed.toUpperCase();
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? 'Sign out' : 'Sign in'}
-      </button>
+    <div>
+      {sessionData ? (
+        <Button
+          variant={'outline'}
+          className="rounded-lg fixed top-4 right-4"
+          onClick={() => void signOut()}
+        >
+          {parseInitials(sessionData.user?.email ?? '')}
+        </Button>
+      ) : (
+        <Button className="mt-4" onClick={() => void signIn()}>
+          Sign in
+        </Button>
+      )}
     </div>
   );
 }
